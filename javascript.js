3a6d6353cfb5c77d81645fbecb18f191a389b981
Erwin Hitgano <163173846+Hitgan0@users.noreply.github.com>
2025-06-20 15:35:09 -0400

    Displayed win and loss messages on screen turned console.log msgs into onscreen text

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getCompChoice() {
     randNum = Math.floor(Math.random() * 3);
-    switch(randNum) {
+    switch (randNum) {
         case 0:
             return "rock";
         case 1:
@@ -10,61 +10,81 @@ function getCompChoice() {
     }
 }
 
-function getHumanChoice() {
-    choice = prompt("Please enter either rock, paper, or scissors: ");
-    return choice;
-}
-
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "rock") {
         if (computerChoice == "paper") {
-            console.log("You lost D:");
+            winOrLose.textContent = "You lost D:"
+            compWon++;
+            compScore.textContent = compWon;
+            // console.log("games played: ", gamesPlayed)
             return 0;
         } else if (computerChoice == "scissors") {
-            console.log("You won :D");
+            winOrLose.textContent = "You won :D";
+            humanWon++;
+            humanScore.textContent = humanWon;
             return 1;
         } else if (computerChoice == "rock") {
-            console.log("You tied :|");
+            winOrLose.textContent = "You tied :|";
             return -1;
         }
     } else if (humanChoice == "paper") {
 
         if (computerChoice == "scissors") {
-            console.log("You lost D:");
+            winOrLose.textContent = "You lost D:";
+            compWon++;
+            compScore.textContent = compWon;
+
             return 0;
         } else if (computerChoice == "rock") {
-            console.log("You won :D");
+            winOrLose.textContent = "You won :D";
+            humanWon++;
+            humanScore.textContent = humanWon;
             return 1;
         } else if (computerChoice == "paper") {
-            console.log("You tied :|");
+            winOrLose.textContent = "You tied :|";
             return -1;
         }
     } else if (humanChoice == "scissors") {
         if (computerChoice == "rock") {
-            console.log("You lost D:");
+            winOrLose.textContent = "You lost D:";
+            compWon++;
+            compScore.textContent = compWon;
             return 0;
         } else if (computerChoice == "paper") {
-            console.log("You won :D");
+            winOrLose.textContent = "You won :D";
+            humanWon++;
+            humanScore.textContent = humanWon;
             return 1;
         } else if (computerChoice == "scissors") {
-            console.log("You tied :|")
+            winOrLose.textContent = "You tied :|";
             return -1;
         }
     }
     return;
 }
 
+const winOrLose = document.querySelector(".winOrLose");
+const humanScore = document.querySelector(".humanScore");
+const compScore = document.querySelector(".compScore");
+
+let gamesPlayed = 0;
+let humanWon = 0;
+let compWon = 0;
+
 const rockB = document.querySelector(".rockBtn");
 rockB.addEventListener("click", () => {
-    playRound("rock", getCompChoice());
+    playRound("rock", "paper");
+    gamesPlayed++;
 });
 
 const paperB = document.querySelector(".paperBtn");
 paperB.addEventListener("click", () => {
     playRound("paper", getCompChoice());
+    gamesPlayed++;
 });
 
 const scissorsB = document.querySelector(".scissorsBtn");
 scissorsB.addEventListener("click", () => {
     playRound("scissors", getCompChoice());
+    gamesPlayed++;
 });
